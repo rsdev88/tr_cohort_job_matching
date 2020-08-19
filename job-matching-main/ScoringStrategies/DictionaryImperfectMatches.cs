f057a001d8626af1f145f5b719adc0f422ea2c7b
Rob Sangar <robsangar@gmail.com>
2020-08-19 15:15:01 +0100

    Indicated what the next incrementation of my solution would be, given the time.

---
+++ job-matching-main/ScoringStrategies/DictionaryImperfectMatches.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace job_matching_main.ScoringStrategies
+{
+
+    //*************************
+    // I ran out of time but would would aim to compare against a white list of accepted terms for non-peferct matching.
+    //***********************
+
+    //Uses a similar strategy to the DictionaryPerfectMatch approach but does not require perfect matches between the characteristics
+    //Pros - more time efficient than the brute force approach - O(n) time complexity, does not require perfect matches (which are unlikely in production if input strings are free text)
+    //Cons - uses more memory than the brute force approach.
+    public class DictionaryImperfectMatches
+    {
+        public int GenerateScore(List<string> jobCharacteristics, List<string> candidateCharacteristics)
+        {
+            return 100;
+        }
+    }
+}
