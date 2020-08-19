8be405dc6e127fd1ba261270daa46cfe72782e0e
Rob Sangar <robsangar@gmail.com>
2020-08-19 14:19:22 +0100

    Initial unit testing for null or empty inputs
    Improved .gitignore file

---
+++ job-matching-main/JobMatching.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace job_matching_main
+{
+    public class JobMatching
+    {
+        public int MatchJob(List<string> jobCharacteristics, List<string> candidateCharacteristics)
+        {
+            //Input validation - if the job or candidate has no characteristics, assume they do not match and return a score of 0
+            if ((jobCharacteristics == null || jobCharacteristics.Count < 1) || (candidateCharacteristics == null || candidateCharacteristics.Count < 1))
+            {
+                return 0;
+            }
+
+            return 100;
+        }
+    }
+}
---
+++ job-matching-tests/JobMatchingTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using job_matching_main;
+
+namespace job_matching_tests
+{
+    [TestClass]
+    public class JobMatchingTests
+    {
+        [TestMethod]
+        public void NullJobCharacteristicsShouldReturnZero()
+        {
+            //Arrange
+            var jobMatching = new JobMatching();
+            List<string> jobCharacteristics = null;
+            var candidateCharacteristics = new List<string>() { "React", ".NET" };
+
+            var expectedResult = 0;
+
+            //Act
+            var result = jobMatching.MatchJob(jobCharacteristics, candidateCharacteristics);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void NullCandidateCharacteristicsShouldReturnZero()
+        {
+            //Arrange
+            var jobMatching = new JobMatching();
+            var jobCharacteristics = new List<string>() { "Remote", "Flexible hours" };
+            List<string> candidateCharacteristics = null;
+
+            var expectedResult = 0;
+
+            //Act
+            var result = jobMatching.MatchJob(jobCharacteristics, candidateCharacteristics);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void EmptyJobCharacteristicsShouldReturnZero()
+        {
+            //Arrange
+            var jobMatching = new JobMatching();
+            var jobCharacteristics = new List<string>();
+            var candidateCharacteristics = new List<string>() { "React", ".NET" };
+
+            var expectedResult = 0;
+
+            //Act
+            var result = jobMatching.MatchJob(jobCharacteristics, candidateCharacteristics);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void EmptyCandidateCharacteristicsShouldReturnZero()
+        {
+            //Arrange
+            var jobMatching = new JobMatching();
+            var jobCharacteristics = new List<string>() { "Remote", "Flexible hours" };
+            var candidateCharacteristics = new List<string>();
+
+            var expectedResult = 0;
+
+            //Act
+            var result = jobMatching.MatchJob(jobCharacteristics, candidateCharacteristics);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
